2eab00c2ca2c833266d40b23bc7235db3830eea8
phoenix-zero <anubhab.mukherjee2013@gmail.com>
2019-12-26 21:46:55 +0530

    Added ListView

--- app/src/main/java/com/pokedexdb/MainActivity.java
+++ app/src/main/java/com/pokedexdb/MainActivity.java
@@ -1,9 +1,14 @@
 package com.pokedexdb;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.mainactivity);
+        ListView listView = findViewById(R.id.pokelist);
+
+        List<String> values = new ArrayList<>();
+        values.add("Bulbasaur");
+        values.add("Charmander");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, values);
+
+        listView.setAdapter(arrayAdapter);
 
 
     }
