9a18bf4343d05c97b922ff4c09381a77347ca707
phoenix-zero <anubhab.mukherjee2013@gmail.com>
2019-12-26 22:19:26 +0530

    ...

--- app/src/main/java/com/pokedexdb/MainActivity.java
+++ app/src/main/java/com/pokedexdb/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pokedexdb;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,24 +13,52 @@ import androidx.navigation.ui.AppBarConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    //private AppBarConfiguration mAppBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.mainactivity);
-        ListView listView = findViewById(R.id.pokelist);
 
-        List<String> values = new ArrayList<>();
-        values.add("Bulbasaur");
-        values.add("Charmander");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, values);
+        final ListView listView = findViewById(R.id.pokelist);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (MainActivity.this, android.R.layout.simple_list_item_1);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+
+                    PokeApi api = new PokeApiClient();
+
+                    Looper.prepare();
+                    List<String> values = new ArrayList<>();
+                    for (int i=1; i<=5; i++)
+                        values.add(api.getPokemonSpecies(i).getName());
+
+                    arrayAdapter.addAll(values);
+
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
 
         listView.setAdapter(arrayAdapter);
+
+        thread.start();
+
+
 
 
     }
