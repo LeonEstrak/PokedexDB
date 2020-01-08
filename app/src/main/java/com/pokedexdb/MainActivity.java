b996586ede75e27cc6fed336b38e8b7cda84a435
LeonEstrak <aniket2583@gmail.com>
2020-01-08 15:15:58 +0530

    Attempts to add more than 11 pokemon to the list fails

--- app/src/main/java/com/pokedexdb/MainActivity.java
+++ app/src/main/java/com/pokedexdb/MainActivity.java
@@ -1,14 +1,12 @@
 package com.pokedexdb;
 
 import android.os.Bundle;
-import android.os.Looper;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TabHost;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +14,7 @@ import java.util.List;
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     //private AppBarConfiguration mAppBarConfiguration;
     @Override
@@ -31,35 +29,38 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (MainActivity.this, android.R.layout.simple_list_item_1);
 
+        final Button button = findViewById(R.id.button);
+
         Thread thread = new Thread() {
             @Override
             public void run() {
                 try {
-
                     PokeApi api = new PokeApiClient();
-
-                    Looper.prepare();
                     List<String> values = new ArrayList<>();
-                    for (int i=1; i<=5; i++)
+                    for (int i=1; i<=12; i++) { // number greater than 11 are not working
                         values.add(api.getPokemonSpecies(i).getName());
-
+                        button.setText(i); // doesn't do anything for some reason
+                    }
                     arrayAdapter.addAll(values);
-
-
+                    button.setText("Refresh Completed"); // doesn't do anything for some reason
                 }
                 catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         };
-
-        listView.setAdapter(arrayAdapter);
-
         thread.start();
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //button.setText(new PokeApiClient().getPokemon(1).toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
 
 
+        listView.setAdapter(arrayAdapter);
 
     }
 
